dcfe91a0978b07f3a7a31a1dbc733ffa16a3e634
vincentimo <vincentius_timothy@hotmail.com>
2020-09-14 20:44:28 +0700

    Solve apple-and-orange with Go

---
+++ apple-and-orange/main.go
@@ -0,0 +1,115 @@
+// https://www.hackerrank.com/challenges/apple-and-orange
+
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "io"
+  "os"
+  "strconv"
+  "strings"
+)
+
+// Complete the countApplesAndOranges function below.
+func countApplesAndOranges(s int32, t int32, a int32, b int32, apples []int32, oranges []int32) {
+  
+  // Declare and initilize the variables
+  var i32CurrentPosition int32
+  var iAppleCount int = 0
+  var iOrangeCount int = 0
+  
+  // Do the Apple loop
+  for i := 0; i < len(apples); i++ {
+    i32CurrentPosition = a + apples[i]
+    if (i32CurrentPosition >= s) && (i32CurrentPosition <= t) {
+      iAppleCount++
+    }
+  }
+  
+  // Do the Orange loop
+  for i := 0; i < len(oranges); i++ {
+    i32CurrentPosition = b + oranges[i]
+    if (i32CurrentPosition >= s) && (i32CurrentPosition <= t) {
+      iOrangeCount++
+    }
+  }
+  
+  // Print the result
+  fmt.Printf("%d\n", iAppleCount)
+  fmt.Printf("%d\n", iOrangeCount)
+  
+}
+
+func main() {
+  reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+  st := strings.Split(readLine(reader), " ")
+
+  sTemp, err := strconv.ParseInt(st[0], 10, 64)
+  checkError(err)
+  s := int32(sTemp)
+
+  tTemp, err := strconv.ParseInt(st[1], 10, 64)
+  checkError(err)
+  t := int32(tTemp)
+
+  ab := strings.Split(readLine(reader), " ")
+
+  aTemp, err := strconv.ParseInt(ab[0], 10, 64)
+  checkError(err)
+  a := int32(aTemp)
+
+  bTemp, err := strconv.ParseInt(ab[1], 10, 64)
+  checkError(err)
+  b := int32(bTemp)
+
+  mn := strings.Split(readLine(reader), " ")
+
+  mTemp, err := strconv.ParseInt(mn[0], 10, 64)
+  checkError(err)
+  m := int32(mTemp)
+
+  nTemp, err := strconv.ParseInt(mn[1], 10, 64)
+  checkError(err)
+  n := int32(nTemp)
+
+  applesTemp := strings.Split(readLine(reader), " ")
+
+  var apples []int32
+
+  for i := 0; i < int(m); i++ {
+    applesItemTemp, err := strconv.ParseInt(applesTemp[i], 10, 64)
+    checkError(err)
+    applesItem := int32(applesItemTemp)
+    apples = append(apples, applesItem)
+  }
+
+  orangesTemp := strings.Split(readLine(reader), " ")
+
+  var oranges []int32
+
+  for i := 0; i < int(n); i++ {
+    orangesItemTemp, err := strconv.ParseInt(orangesTemp[i], 10, 64)
+    checkError(err)
+    orangesItem := int32(orangesItemTemp)
+    oranges = append(oranges, orangesItem)
+  }
+
+  countApplesAndOranges(s, t, a, b, apples, oranges)
+}
+
+func readLine(reader *bufio.Reader) string {
+  str, _, err := reader.ReadLine()
+  if err == io.EOF {
+    return ""
+  }
+
+  return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+  if err != nil {
+    panic(err)
+  }
+}
